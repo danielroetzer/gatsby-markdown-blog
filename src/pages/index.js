af4a449ed202becb5e950b5cf32bdf230862fa96
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-11-29 12:03:45 +0100

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
+/** EXTERNALS **/
+
 import React from 'react';
 
+/** LOCALS **/
+
+import Layout from '../components/Layout';
+
+/** HELPERS **/
+
+/** MAIN **/
+
 export default function Home() {
-    return <div>Hello world!</div>;
+    return (
+        <Layout>
+            <div>Home Page</div>
+        </Layout>
+    );
 }
