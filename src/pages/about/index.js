51f2b9e81ac82b24f923c9b7652ee284e4f31d88
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-11-29 12:03:59 +0100

    add projects and about pages

---
+++ src/pages/about/index.js
@@ -0,0 +1,21 @@
+/** EXTERNALS **/
+
+import React from 'react';
+
+/** LOCALS **/
+
+import Layout from '../../components/Layout';
+
+/** HELPERS **/
+
+/** MAIN **/
+
+const AboutPage = function () {
+    return (
+        <Layout>
+            <div>About Page</div>
+        </Layout>
+    );
+};
+
+export default AboutPage;
---
+++ src/pages/projects/index.js
@@ -0,0 +1,21 @@
+/** EXTERNALS **/
+
+import React from 'react';
+
+/** LOCALS **/
+
+import Layout from '../../components/Layout';
+
+/** HELPERS **/
+
+/** MAIN **/
+
+const ProjectsPage = function () {
+    return (
+        <Layout>
+            <div>Projects Page</div>
+        </Layout>
+    );
+};
+
+export default ProjectsPage;
