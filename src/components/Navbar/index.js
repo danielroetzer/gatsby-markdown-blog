566c0af4096b5c516adf885418ab52ddeb8ed656
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-11-29 15:04:58 +0100

    set `Navbar` title via `siteMetadata`

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,12 +2,23 @@
 
 import React from 'react';
 import { Link } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 /** LOCALS **/
 
 import * as styles from './Navbar.module.css';
 
 /** HELPERS **/
+
+const query = graphql`
+    query Navbar {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`;
 
 const activeLinkStyle = {
     textDecoration: 'underline',
@@ -16,10 +27,13 @@ const activeLinkStyle = {
 /** MAIN **/
 
 const Navbar = function () {
+    const { site } = useStaticQuery(query);
+    const { title } = site.siteMetadata;
+
     return (
         <div className={styles.navbar}>
-            <Link to="/" className={styles.brand}>
-                <h2>Gatsby Blog</h2>
+            <Link to="/" className={styles.title}>
+                <h2>{title}</h2>
             </Link>
 
             <div className={styles.links}>
