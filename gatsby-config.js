597d46df241b4626ac9e48472e5096b5b5794451
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-11-29 15:14:03 +0100

    set `Footer` author via `siteMetadata`

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+/** EXTERNALS **/
 
+/** LOCALS **/
+
+const package = require('./package.json');
+
+/** HELPERS **/
+
+/** MAIN **/
+
+// https://www.gatsbyjs.com/docs/gatsby-config/
 module.exports = {
-    /* Your site config here */
     plugins: ['gatsby-plugin-react-helmet'],
     siteMetadata: {
         title: 'Gatsby Markdown Blog',
         titleTemplate: '%s | Gatsby Markdown Blog',
         description: 'Gatsby blog sourcing from markdown files.',
+        author: package.author.name,
     },
 };
